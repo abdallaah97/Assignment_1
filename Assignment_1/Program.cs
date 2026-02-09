d7ad83ba314c4cda88d1471cd06e27470154902a
Abduallah Almara'beh <aalmarabeh@sedrapay.com>
2026-02-09 20:33:39 +0300

    new code

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Switch
+Console.WriteLine("Enter number 1 : ");
+double num_1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number 2 : ");
+double num_2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter the operator : ");
+char @operator = Convert.ToChar(Console.ReadLine());
+
+switch (@operator)
+{
+    case '+':
+        Console.WriteLine($"The result of opertor {@operator} for {num_1} and {num_2} is : {num_1 + num_2}");
+        break;
+
+    case '-':
+        Console.WriteLine($"The result of opertor {@operator} for {num_1} and {num_2} is : {num_1 - num_2}");
+        break;
+
+    case '*':
+        Console.WriteLine($"The result of opertor {@operator} for {num_1} and {num_2} is : {num_1 * num_2}");
+        break;
+
+    case '/':
+        Console.WriteLine($"The result of opertor {@operator} for {num_1} and {num_2} is : {num_1 / num_2}");
+        break;
+
+    default:
+        Console.WriteLine($"Invalid operator");
+        break;
+}
